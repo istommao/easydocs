ef8f578d6c6454b517c585e185f001339ff4e817
istommao <istommao@gmail.com>
2018-07-15 18:25:20 +0800

    Add project detail page

--- server/project/urls.py
+++ server/project/urls.py
@@ -1,9 +1,11 @@
 """project urls."""
-from django.urls import path
+from django.urls import path, re_path
 
-from project.views import ProjectListView
+from project.views import ProjectListView, ProjectDetailView
 
 app_name = 'project'
+
 urlpatterns = [
-    path('', ProjectListView.as_view(), name='index')
+    path('', ProjectListView.as_view(), name='index'),
+    re_path(r'(?P<uid>\w+)/', ProjectDetailView.as_view(), name='detail'),
 ]
--- server/project/views.py
+++ server/project/views.py
@@ -1,6 +1,8 @@
 """project views."""
 from django.views import generic
 
+from project.models import Project
+
 
 class ProjectListView(generic.TemplateView):
 
@@ -9,4 +11,22 @@ class ProjectListView(generic.TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        projects = Project.objects.all()
+
+        context.update({'projects': projects})
+        return context
+
+
+class ProjectDetailView(generic.TemplateView):
+
+    view_name = 'project_detail'
+    template_name = 'project/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        uid = kwargs['uid']
+        project = Project.objects.filter(uid=uid).first()
+
+        context.update({'project': project})
         return context
